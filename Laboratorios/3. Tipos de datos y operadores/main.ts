7be6be53c0372206ee6631c159a3470881488a5e
mbjabalquinto <124250038+mbjabalquinto@users.noreply.github.com>
2023-10-06 17:31:42 +0200

    Update main.ts
    Faltaba un ;

--- Laboratorios/3. Tipos de datos y operadores/main.ts
+++ Laboratorios/3. Tipos de datos y operadores/main.ts
@@ -1,11 +1,11 @@
 import "./style.css";
 
-console.log("Asumo que yo formo parte del grupo de amigos con lo cual somos 6 en total.")
-console.log("Yo pago las bebidas así que el importe de la comida sería:")
+console.log("Asumo que yo formo parte del grupo de amigos con lo cual somos 6 en total.");
+console.log("Yo pago las bebidas así que el importe de la comida sería:");
 console.log(120 - 18);
-console.log("El importe de la comida dividido entre los 6 amigos:")
+console.log("El importe de la comida dividido entre los 6 amigos:");
 console.log(102 / 6);
-console.log("Cada amigo tiene que pagar 17 euros por la comida ya que yo he invitado a las bebidas.")
+console.log("Cada amigo tiene que pagar 17 euros por la comida ya que yo he invitado a las bebidas.");
 
 
 
