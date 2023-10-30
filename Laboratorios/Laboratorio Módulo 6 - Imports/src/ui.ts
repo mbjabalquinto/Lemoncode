c8a1aa275bedcbcc8baf8394cf32c47c7298b8d5
Marcos <mbjabalquinto@gmail.com>
2023-10-30 14:39:10 +0100

    añado fichero shell.ts y muevo eventos a la ui.

--- Laboratorios/Laboratorio Módulo 6 - Imports/src/ui.ts
+++ Laboratorios/Laboratorio Módulo 6 - Imports/src/ui.ts
@@ -157,3 +157,39 @@ export const mePlanto = () => {
   activaBoton("nueva");
   activaBoton("continuar");
 };
+
+// MANEJADOR DE EVENTOS. MANEJAMOS LOS 4 BOTONES.
+export const manejaEventos = (): void => {
+  const botonCarta = document.getElementById("dameCarta");
+  if (
+    botonCarta !== null &&
+    botonCarta !== undefined &&
+    botonCarta instanceof HTMLButtonElement
+  ) {
+    botonCarta.addEventListener("click", dameCarta);
+  }
+  const botonPlanto = document.getElementById("meplanto");
+  if (
+    botonPlanto !== null &&
+    botonPlanto !== undefined &&
+    botonPlanto instanceof HTMLButtonElement
+  ) {
+    botonPlanto.addEventListener("click", mePlanto);
+  }
+  const botonNueva = document.getElementById("nueva");
+  if (
+    botonNueva !== null &&
+    botonNueva !== undefined &&
+    botonNueva instanceof HTMLButtonElement
+  ) {
+    botonNueva.addEventListener("click", nuevaPartida);
+  }
+  const seguir = document.getElementById("continuar");
+  if (
+    seguir !== null &&
+    seguir !== undefined &&
+    seguir instanceof HTMLButtonElement
+  ) {
+    seguir.addEventListener("click", whatIf);
+  }
+};
