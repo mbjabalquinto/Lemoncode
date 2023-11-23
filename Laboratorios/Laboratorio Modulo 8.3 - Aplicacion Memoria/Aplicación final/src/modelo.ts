049b5f8e91fd18cec4eebe646a632da502cacdd7
Marcos <mbjabalquinto@gmail.com>
2023-11-23 18:29:40 +0100

    comienzo el juego.

---
+++ Laboratorios/Laboratorio Modulo 8.3 - Aplicacion Memoria/Aplicación final/src/modelo.ts
@@ -0,0 +1,131 @@
+export interface Cartas {
+  idFoto: number;
+  imagen: string;
+  estaVuelta: boolean;
+  encontrada: boolean;
+}
+
+interface InfoCarta {
+  idFoto: number;
+  imagen: string;
+}
+
+const infoCartas: InfoCarta[] = [
+  {
+    idFoto: 1,
+    imagen:
+      "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/memo/1.png",
+  },
+  {
+    idFoto: 2,
+    imagen:
+      "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/memo/2.png",
+  },
+  {
+    idFoto: 3,
+    imagen:
+      "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/memo/3.png",
+  },
+  {
+    idFoto: 4,
+    imagen:
+      "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/memo/4.png",
+  },
+  {
+    idFoto: 5,
+    imagen:
+      "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/memo/5.png",
+  },
+  {
+    idFoto: 6,
+    imagen:
+      "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/memo/6.png",
+  },
+];
+
+export const cartas: Cartas[] = [
+  {
+    idFoto: 1,
+    imagen:
+      "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/memo/1.png",
+    estaVuelta: false,
+    encontrada: false,
+  },
+  {
+    idFoto: 2,
+    imagen:
+      "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/memo/2.png",
+    estaVuelta: false,
+    encontrada: false,
+  },
+  {
+    idFoto: 3,
+    imagen:
+      "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/memo/3.png",
+    estaVuelta: false,
+    encontrada: false,
+  },
+  {
+    idFoto: 4,
+    imagen:
+      "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/memo/4.png",
+    estaVuelta: false,
+    encontrada: false,
+  },
+  {
+    idFoto: 5,
+    imagen:
+      "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/memo/5.png",
+    estaVuelta: false,
+    encontrada: false,
+  },
+  {
+    idFoto: 6,
+    imagen:
+      "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/memo/6.png",
+    estaVuelta: false,
+    encontrada: false,
+  },
+  {
+    idFoto: 1,
+    imagen:
+      "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/memo/1.png",
+    estaVuelta: false,
+    encontrada: false,
+  },
+  {
+    idFoto: 2,
+    imagen:
+      "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/memo/2.png",
+    estaVuelta: false,
+    encontrada: false,
+  },
+  {
+    idFoto: 3,
+    imagen:
+      "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/memo/3.png",
+    estaVuelta: false,
+    encontrada: false,
+  },
+  {
+    idFoto: 4,
+    imagen:
+      "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/memo/4.png",
+    estaVuelta: false,
+    encontrada: false,
+  },
+  {
+    idFoto: 5,
+    imagen:
+      "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/memo/5.png",
+    estaVuelta: false,
+    encontrada: false,
+  },
+  {
+    idFoto: 6,
+    imagen:
+      "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/memo/6.png",
+    estaVuelta: false,
+    encontrada: false,
+  },
+];
---
+++ Laboratorios/Laboratorio Modulo 8.3 - Aplicacion Memoria/Prueba de concepto 5 - Mapear el DIV que contiene la carta con la posición del array de las cartas/src/main.ts
@@ -0,0 +1,37 @@
+interface Cartas {
+  idFoto: number;
+  imagen: string;
+}
+
+const cartas: Cartas[] = [
+  {
+    idFoto: 1,
+    imagen:
+      "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/memo/1.png",
+  },
+  {
+    idFoto: 2,
+    imagen:
+      "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/memo/2.png",
+  },
+  {
+    idFoto: 3,
+    imagen:
+      "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/memo/3.png",
+  },
+  {
+    idFoto: 4,
+    imagen:
+      "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/memo/4.png",
+  },
+  {
+    idFoto: 5,
+    imagen:
+      "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/memo/5.png",
+  },
+  {
+    idFoto: 6,
+    imagen:
+      "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/memo/6.png",
+  },
+];
