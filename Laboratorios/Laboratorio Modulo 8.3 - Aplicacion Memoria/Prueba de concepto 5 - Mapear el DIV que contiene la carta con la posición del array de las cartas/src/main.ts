4b760faac74c65865bdfd1b24b54f39c9e57eba8
Marcos <mbjabalquinto@gmail.com>
2023-12-04 18:17:50 +0100

    añado código al concepto 5.

--- Laboratorios/Laboratorio Modulo 8.3 - Aplicacion Memoria/Prueba de concepto 5 - Mapear el DIV que contiene la carta con la posición del array de las cartas/src/main.ts
+++ Laboratorios/Laboratorio Modulo 8.3 - Aplicacion Memoria/Prueba de concepto 5 - Mapear el DIV que contiene la carta con la posición del array de las cartas/src/main.ts
@@ -35,3 +35,51 @@ const cartas: Cartas[] = [
       "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/memo/6.png",
   },
 ];
+
+const compruebaCarta = (indice: number): void => {
+  const carta = document.getElementById(`carta${indice}`);
+  if (
+    carta !== null &&
+    carta !== undefined &&
+    carta instanceof HTMLImageElement
+  ) {
+    carta.src = cartas[indice].imagen;
+  }
+};
+
+const eventos = () => {
+  const carta1 = document.getElementById("contenedor0");
+  if (
+    carta1 !== null &&
+    carta1 !== undefined &&
+    carta1 instanceof HTMLDivElement
+  ) {
+    carta1.addEventListener("click", (event) => {
+      if (event.currentTarget instanceof HTMLElement) {
+        const indice1 = Number(
+          event.currentTarget.getAttribute("data-indice-array")
+        );
+        compruebaCarta(indice1);
+        console.log(indice1);
+      }
+    });
+  }
+  const carta2 = document.getElementById("contenedor1");
+  if (
+    carta2 !== null &&
+    carta2 !== undefined &&
+    carta2 instanceof HTMLDivElement
+  ) {
+    carta2.addEventListener("click", (event) => {
+      if (event.currentTarget instanceof HTMLElement) {
+        const indice2 = Number(
+          event.currentTarget.getAttribute("data-indice-array")
+        );
+        compruebaCarta(indice2);
+        console.log(indice2);
+      }
+    });
+  }
+};
+
+document.addEventListener("DOMContentLoaded", eventos);
